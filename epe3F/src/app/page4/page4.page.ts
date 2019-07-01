6a9d6de84650e0ee342cc92863f8837ee7e3e735
Darknees1997 <38507376+Darknees1997@users.noreply.github.com>
2019-07-01 03:52:41 -0400

    Update page4.page.ts

--- epe3F/src/app/page4/page4.page.ts
+++ epe3F/src/app/page4/page4.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-page4',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page4Page implements OnInit {
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
   }
-
+  imagen1(){
+    this.navCtrl.navigateForward("img4");
+    }
 }
