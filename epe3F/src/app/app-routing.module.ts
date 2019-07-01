1df9cae1a92c2d722039c7c0569a6703b28b6d9b
Darknees1997 <38507376+Darknees1997@users.noreply.github.com>
2019-07-01 01:38:39 -0400

    page4

--- epe3F/src/app/app-routing.module.ts
+++ epe3F/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   { path: 'page1', loadChildren: './page1/page1.module#Page1PageModule' },
   { path: 'page2', loadChildren: './page2/page2.module#Page2PageModule' },
   { path: 'page3', loadChildren: './page3/page3.module#Page3PageModule' },
+  { path: 'page4', loadChildren: './page4/page4.module#Page4PageModule' },
 ];
 
 @NgModule({
