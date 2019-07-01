267cc338a36bd3adb4596d6a36aed3f91a6577aa
Darknees1997 <38507376+Darknees1997@users.noreply.github.com>
2019-07-01 01:30:51 -0400

    Update pagina2.page.ts

--- epe3F/src/app/pagina2/pagina2.page.ts
+++ epe3F/src/app/pagina2/pagina2.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pagina2',
